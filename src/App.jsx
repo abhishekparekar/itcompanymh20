0709ddb6e67ddb8803454e3685487effb83d5d0d
abhishek parekar <abhiparekar58@gmail.com>
2026-07-10 15:30:06 +0530

    fix: redirect invalid routes to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import MainLayout from './layouts/MainLayout';
 
@@ -60,7 +60,7 @@ function App() {
           />
 
           {/* Fallback route */}
-          <Route path="*" element={<MainLayout><Home /></MainLayout>} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
     </AuthProvider>
